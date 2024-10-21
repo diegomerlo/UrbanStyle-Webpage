7b44cf253ac3ac02e6d65147ed85d6e7522cbd00
lautaropereiras <lautaropereiras1705@gmail.com>
2024-10-21 19:15:08 -0300

    3:24:00

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './componentes/Navbar';
 import CheckOutPage from './componentes/CheckOutPage';
 import CheckoutCard from './componentes/CheckOutCard';
 import Productos from './componentes/Productos';  // Mantén esta importación como componente
+import  SingIn  from "./componentes/SingIn";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
       <div className="App" style={{ paddingTop: '70px' }}>
         <Navbar />
         <Routes>
+        <Route path='/SingIn' element={<SingIn />}>
+        </Route>
           <Route path='/checkout-page' element={<CheckOutPage />}>
           </Route>
           <Route path='/' element={<Productos />} >
